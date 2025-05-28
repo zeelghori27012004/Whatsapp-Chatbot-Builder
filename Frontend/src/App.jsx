a22c542cd2ca6028c596db5cd7085fca9c21fa03
D-Godhani <dharmikgodhani0@gmail.com>
2025-05-28 14:41:04 +0530

    Installed Dependencies

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+
+function App() {
+  return <h1 className="text-red-100 font-extralight"> HELLO WORLD</h1>;
+}
+
+export default App;
