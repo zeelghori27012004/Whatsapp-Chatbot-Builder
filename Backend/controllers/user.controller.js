18a11c4358cb31543a80dd3b7b0548bb255adbd2
zeelghori27012004 <ghorizeeljkvrz@gmail.com>
2025-06-02 22:39:40 +0530

    installed redix

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,7 +1,7 @@
 import userModel from "../models/user.model.js";
 import * as userService from "../services/user.service.js";
 import { validationResult } from "express-validator";
-// import redisClient from '../services/redis.service.js';
+import redisClient from '../services/redis.service.js';
 
 export const createUserController = async (req, res) => {
   const errors = validationResult(req);
