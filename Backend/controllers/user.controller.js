0a5ab5b703afe137f337e7cfbb543889d60e437e
zeelghori27012004 <ghorizeeljkvrz@gmail.com>
2025-06-02 16:41:50 +0530

    added user services and changes in user controller

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,70 +1,115 @@
-import bcrypt from "bcryptjs";
-import { User } from "../models/User.js";
-import jwt from "jsonwebtoken";
-import generateToken from "../utils/generateToken.js";
+import userModel from '../models/user.model.js';
+import * as userService from '../services/user.service.js';
+import { validationResult } from 'express-validator';
+import redisClient from '../services/redis.service.js';
 
-export const register = async (req, res) => {
-  const { fullname, email, phoneNumber, password, isAdmin } = req.body;
-  if (!fullname || !email || !phoneNumber || !password) {
-    return res.status(400).json({
-      message: "Something is missing",
-      success: false,
+
+export const createUserController = async (req, res) => {
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+        const user = await userService.createUser(req.body);
+
+        const token = await user.generateJWT();
+
+        delete user._doc.password;
+
+        res.status(201).json({ user, token });
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+}
+
+export const loginController = async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+
+        const { email, password } = req.body;
+
+        const user = await userModel.findOne({ email }).select('+password');
+
+        if (!user) {
+            return res.status(401).json({
+                errors: 'Invalid credentials'
+            })
+        }
+
+        const isMatch = await user.isValidPassword(password);
+
+        if (!isMatch) {
+            return res.status(401).json({
+                errors: 'Invalid credentials'
+            })
+        }
+
+        const token = await user.generateJWT();
+
+        delete user._doc.password;
+
+        res.status(200).json({ user, token });
+
+
+    } catch (err) {
+
+        console.log(err);
+
+        res.status(400).send(err.message);
+    }
+}
+
+export const profileController = async (req, res) => {
+
+    res.status(200).json({
+        user: req.user
     });
-  }
-  const user = await User.findOne({ email });
-  if (user) {
-    return res.status(400).json({
-      message: "User already exist with this email.",
-      success: false,
-    });
-  }
-  const hashedPassword = await bcrypt.hash(password, 10);
 
-  const newuser = await User.create({
-    fullname,
-    email,
-    phoneNumber,
-    password: hashedPassword,
-    isAdmin,
-  });
+}
 
-  return res.status(201).json({
-    message: "Account created successfully.",
-    success: true,
-  });
+export const logoutController = async (req, res) => {
+    try {
 
-  res.json({ token: generateToken(newuser._id) });
-};
+        const token = req.cookies.token || req.headers.authorization.split(' ')[ 1 ];
 
-export const login = async (req, res) => {
-  const { email, password, isAdmin } = req.body;
-  if (!email || !password) {
-    return res.status(400).json({
-      message: "Something is missing",
-      success: false,
-    });
-  }
-  let user = await User.findOne({ email });
-  if (!user) {
-    return res.status(400).json({
-      message: "Incorrect email or password.",
-      success: false,
-    });
-  }
-  const isPasswordMatch = await bcrypt.compare(password, user.password);
-  if (!isPasswordMatch) {
-    return res.status(400).json({
-      message: "Incorrect email or password.",
-      success: false,
-    });
-  }
+        redisClient.set(token, 'logout', 'EX', 60 * 60 * 24);
 
-  if (isAdmin !== user.isAdmin) {
-    return res.status(400).json({
-      message: "Account doesn't exist with current role.",
-      success: false,
-    });
-  }
+        res.status(200).json({
+            message: 'Logged out successfully'
+        });
 
-  res.json({ token: generateToken(user._id) });
-};
+
+    } catch (err) {
+        console.log(err);
+        res.status(400).send(err.message);
+    }
+}
+
+export const getAllUsersController = async (req, res) => {
+    try {
+
+        const loggedInUser = await userModel.findOne({
+            email: req.user.email
+        })
+
+        const allUsers = await userService.getAllUsers({ userId: loggedInUser._id });
+
+        return res.status(200).json({
+            users: allUsers
+        })
+
+    } catch (err) {
+
+        console.log(err)
+
+        res.status(400).json({ error: err.message })
+
+    }
+}
