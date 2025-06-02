bd43a90ac774658027c0131d1dcbdc5e58ba62dd
zeelghori27012004 <ghorizeeljkvrz@gmail.com>
2025-06-02 16:27:37 +0530

    final changes in user schema

--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -23,6 +23,7 @@ const userSchema = new mongoose.Schema(
     },
     password: {
       type: String,
+      select: false, 
       required: true,
     },
     isAdmin: { type: Boolean, default: false },
